3931d2af61ee1f46113eb81e42647889b26fa7b4
Omoyele Samuel <samuelomoyele777@gmail.com>
2022-02-14 00:50:55 +0100

    update

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -198,5 +198,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-if os.getcwd == '/app':
+if os.getcwd() == '/app':
     DEBUG = False
