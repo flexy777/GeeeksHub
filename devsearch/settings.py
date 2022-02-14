08b4335fc767a6bfd57db6952812d127a2d79892
Omoyele Samuel <samuelomoyele777@gmail.com>
2022-02-14 02:42:59 +0100

    update

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ne1+vm8d*u44x3*1-n_&#j#tt6*3a#@6fq+75g3c)laprc=86_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'devsearch7.herokuapp.com', 'devsearch7.netlify.app']
 
