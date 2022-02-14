72d91bded12c5d1b1f8ff94dfd5e035ea486f8b0
Omoyele Samuel <samuelomoyele777@gmail.com>
2022-02-14 10:13:21 +0100

    update

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -123,21 +123,21 @@ WSGI_APPLICATION = 'devsearch.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES ={
-#     'default' : {
-#         'ENGINE' : 'django.db.backends.postgresql',
-#         'NAME' : 'devsearch',
-#         'USER' : 'postgres',
-#         'PASSWORD' : 'sammylee1',
-#         'HOST' : 'localhost',
-#         'PORT': '5432',
-#     }
-# }
- 
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default' : {
+        'ENGINE' : 'django.db.backends.postgresql',
+        'NAME' : 'devsearch',
+        'USER' : 'postgres',
+        'PASSWORD' : 'sammylee1',
+        'HOST' : 'localhost',
+        'PORT': '5432',
     }
 }
+ 
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
