e6ed30d0d4ce30fcff8e8d26e57217ef051f4298
Omoyele Samuel <samuelomoyele777@gmail.com>
2022-02-15 17:33:00 +0100

    update

--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.loginUser, name='login'),
+    path('login/', views.loginUser, name='login'),
     path('logout/', views.logoutUser, name='logout'),
     path('register/', views.registerUser, name='register'),
-    path('profiles', views.profiles, name='profiles'),
+    path('', views.profiles, name='profiles'),
     path('profile/<str:pk>/', views.userProfile, name="user-profile"),
     path('account/', views.userAccount, name="account"),
     path('edit-account/', views.editAccount, name="edit-account"),
